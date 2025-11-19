bfe37a4fdf18da4b13204e01e08441e4bf511dbe
AdrianCa17-G <adrian.steven.guaman.carrion@gmail.com>
2025-11-19 12:22:32 -0500

    segundo commit

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
@@ -21,7 +21,7 @@ public class Ejemplo02 {
         int limite;
         int contador = 1;
         System.out.println("Ingrese el Límite");
-        limite = entrada.nextInt(); 
+        limite = entrada.nextInt(); // 36
         while (contador <= limite){
             
             if (contador%2!=0) {
@@ -36,5 +36,12 @@ public class Ejemplo02 {
 }
 /*
     El programa pide al usuario ingresar un limite de numeros
-    en el cual dentro de ese limite 
+    en el cual dentro de ese limite mostrara los numeros
+    impares hasta llegar a ese limite, ejemplo, si el usuario
+    ingresa 10 el programa presentara los numeros 1 3 5 7 9
+    que son impares hasta que llegue al limite o cerca al
+    limite de 10, el programa evalua mediante el residuo donde
+    ve si el residuo es 1 entonces lo presenta en pantalla,
+    si el residuo es 0 no lo presentara y pasara al siguiente 
+    numero
 */
